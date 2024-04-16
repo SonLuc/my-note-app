23e2cb45816628c3f9f3d7519df4f988a0faba2c
Luciano <luciano.denis.a@gmail.com>
2024-04-16 00:37:58 -0400

    Add export file icon

---
+++ src/components/Icons.tsx
@@ -0,0 +1,30 @@
+import BookIcon from './icons/BookIcon'
+import { MoonIcon, SunIcon } from './icons/DarkModeIcons'
+import ElipsisIcon from './icons/ElipsisIcon'
+import EnvelopeIcon from './icons/EnvelopeIcon'
+import FileCirclesPlusIcon from './icons/FileCirclePlusIcon'
+import HomeIcon from './icons/HomeIcon'
+import LogoutIcon from './icons/LogoutIcon'
+import ToothIcon from './icons/ToothIcon'
+import LockIcon from './icons/LockIcon'
+import UserIcon from './icons/UserIcon'
+import DocumentIcon from './icons/DocumentIcon'
+import CheckCircleIcon from './icons/CheckCircle'
+import PlusIcon from './icons/PlusIcon'
+
+export {
+  BookIcon,
+  MoonIcon,
+  SunIcon,
+  ElipsisIcon,
+  EnvelopeIcon,
+  FileCirclesPlusIcon,
+  HomeIcon,
+  LogoutIcon,
+  ToothIcon,
+  LockIcon,
+  UserIcon,
+  DocumentIcon,
+  CheckCircleIcon,
+  PlusIcon
+}
